fa6a090b6d82acebcd42951fa12432cf7a84a818
Nikita Veko <n.veko@iac.spb.ru>
2023-01-17 21:53:17 +0300

    17.01.2023

--- src/main/java/ru/sbercources/library/repository/AuthorRepository.java
+++ src/main/java/ru/sbercources/library/repository/AuthorRepository.java
@@ -1,5 +1,6 @@
 package ru.sbercources.library.repository;
 
+import java.util.List;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,7 @@ import ru.sbercources.library.model.Book;
 public interface AuthorRepository extends GenericRepository<Author> {
 
   Set<Author> findAllByIdIn(Set<Long> ids);
+
+  List<Author> findAllByAuthorFIO(String authorFIO);
 
 }
--- src/main/java/ru/sbercources/library/rest/controller/PublishController.java
+++ src/main/java/ru/sbercources/library/rest/controller/PublishController.java
@@ -1,17 +1,19 @@
-//package ru.sbercources.library.rest.controller;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import ru.sbercources.library.model.Publish;
-//import ru.sbercources.library.service.GenericService;
-//
-//@Slf4j
-//@RestController
-//@RequestMapping("/rest/publish")
-//public class PublishController extends GenericController<Publish> {
-//
-//  protected PublishController(GenericService<Publish> service) {
-//    super(service, mapper);
-//  }
-//}
+package ru.sbercources.library.rest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sbercources.library.dto.PublishDto;
+import ru.sbercources.library.mapper.PublishMapper;
+import ru.sbercources.library.model.Publish;
+import ru.sbercources.library.service.GenericService;
+
+@Slf4j
+@RestController
+@RequestMapping("/rest/publish")
+public class PublishController extends GenericController<Publish, PublishDto> {
+
+  protected PublishController(GenericService<Publish> service, PublishMapper mapper) {
+    super(service, mapper);
+  }
+}
